01edce6e2a0ec8fe8ef85c1caf0a179fb72a713e
Joao Lucas Gonzaga <joaolucasgos2030@gmail.com>
2026-07-08 20:52:31 -0300

    atualizaçao variaveis

---
+++ JoaoLucas/variaveis_js/variaveis.js
@@ -0,0 +1,8 @@
+const nome  = "Joao Lucas"
+let idade = 18
+let cidade = "uberlândia"
+let profissão = "analista de qualidade"
+console.log(nome)
+console.log(idade)
+console.log(cidade)
+console.log(profissão)
