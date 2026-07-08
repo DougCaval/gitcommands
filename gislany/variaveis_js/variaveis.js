b2763ed1e94f139a49a1b7b6664c41fb79732cb5
Gislany <gislany@gmail.com>
2026-07-08 20:49:46 -0300

    Adiciona exemplos dos tipos de variáveis

--- gislany/variaveis_js/variaveis.js
+++ gislany/variaveis_js/variaveis.js
@@ -2,3 +2,10 @@ let nome = "Gislany";
 let idade = 40;
 let cidade = "Uberlândia";
 let profissao = "Project Manager";
+
+//Exercicio 2
+let texto = "Olá";
+let numero = 10;
+let ativo = true;
+let vazio = null;
+let semValor;
