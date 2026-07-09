058020cd983fd85059d051a6bdf724bc425e5f8a
Joao Lucas Gonzaga <joaolucasgos2030@gmail.com>
2026-07-08 21:13:12 -0300

    exercicio 2

--- JoaoLucas/variaveis_js/variaveis.js
+++ JoaoLucas/variaveis_js/variaveis.js
@@ -1,8 +1,24 @@
-const nome  = "Joao Lucas"
-let idade = 18
-let cidade = "uberlândia"
-let profissão = "analista de qualidade"
-console.log(nome)
-console.log(idade)
-console.log(cidade)
-console.log(profissão)
+//EXERCICIO 1
+//const nome  = "Joao Lucas" 
+//let idade = 18
+//let cidade = "uberlândia"
+//let profissão = "analista de qualidade"
+//console.log(nome)
+//console.log(idade)
+//console.log(cidade)
+//console.log(profissão)
+
+
+//-----------------------------------------------
+
+//EXERCICIO 2
+let nomeJogador = "Jogador1";
+let nivel = 23;
+let estaVivo = true;
+let itemEquipado = null;
+let proximaFase;
+console.log(typeof nomeJogador);
+console.log(typeof nivel);
+console.log(typeof estaVivo);
+console.log(typeof itemEquipado);
+console.log(typeof  proximaFase);
