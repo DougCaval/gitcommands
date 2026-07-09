6e3f0ca04f4630fc9d9d306421ad70f25096b1c6
Joao Lucas Gonzaga <joaolucasgos2030@gmail.com>
2026-07-09 14:12:59 -0300

    desafio da aula

--- JoaoLucas/variaveis_js/variaveis.js
+++ JoaoLucas/variaveis_js/variaveis.js
@@ -23,11 +23,26 @@
 //console.log(typeof itemEquipado);
 //console.log(typeof  proximaFase);
 //exercicio 3
+//const nome = "Joao Lucas";
+//let idade = 18;
+//const altura = 1.78;
+//let possuiCNH = true;
+//console.log("nome: " + nome);
+//console.log("idade: " + idade);
+//console.log("altura: " + altura);
+//console.log("possui CNH: " + possuiCNH);
+// desafio da aula
 const nome = "Joao Lucas";
 let idade = 18;
-const altura = 1.78;
-let possuiCNH = true;
-console.log("nome: " + nome);
-console.log("idade: " + idade);
-console.log("altura: " + altura);
-console.log("possui CNH: " + possuiCNH);
+let cidade = "Uberlândia";
+let profissao = "Analista de Qualidade";
+let empresa = "Lavac IT Tech™";
+let salario = 3500;
+let estaEstudando = true;
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Cidade: " + cidade);
+console.log("Profissão: " + profissao);
+console.log("Empresa: " + empresa);
+console.log("Salário: R$" + salario);
+console.log("Está estudando: " + estaEstudando);
