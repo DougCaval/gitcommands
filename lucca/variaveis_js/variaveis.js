ab0a1ce8d23e235624fb8ad1d53b056fd71d7d9b
Lucca <luccalorenzs@gmail.com>
2026-07-08 21:05:15 -0300

    desafios

---
+++ lucca/variaveis_js/variaveis.js
@@ -0,0 +1,41 @@
+let nome = "Lucca"
+let idade = 25
+let cidade = "Capitólio"
+let profissao = "Developer"
+console.log("---OUTPUT DESAFIO 1:---")
+console.log("Opa, eu sou o " + nome + ". Tenho " + idade + " anos, moro em " + cidade + " e trabalho como " + profissao + ".")
+
+let comidaFavorita = "Pizza"
+let altura = 1.75
+let gostaDeGatos = true
+let gostaDeGatosoutput = gostaDeGatos ? "gosto" : "não gosto"
+let carteira = null
+let hobbies;
+
+console.log("---OUTPUT DESAFIO 2:---")
+console.log("Minha comida favorita é " + comidaFavorita + ". Minha altura é " + altura + " metros. Eu " + gostaDeGatosoutput + " de gatos.")
+console.log("-----------------------")
+console.log(typeof comidaFavorita)
+console.log(typeof altura)
+console.log(typeof gostaDeGatos)
+console.log(typeof carteira)
+console.log(typeof hobbies)
+
+let possuiCnh = true
+let possuiCnhoutput = possuiCnh ? "Sim" : "Não"
+console.log("---OUTPUT DESAFIO 3:---")
+console.log("Nome: " + nome)
+console.log("Idade: " + idade)
+console.log("Altura: " + altura)
+console.log("Possui CNH: " + possuiCnhoutput)
+console.log("---OUTPUT DESAFIO 4:---")
+
+let estaEstudando = true
+let estaEstudandooutput = estaEstudando ? "Sim" : "Não"
+let empresa = "McDonald's"
+console.log("Nome: " + nome)
+console.log("Idade: " + idade)
+console.log("Cidade: " + cidade)
+console.log("Profissão: " + profissao)
+console.log("Empresa: " + empresa)
+console.log("Está estudando: " + estaEstudandooutput)
