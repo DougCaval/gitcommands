489bb1e4f95c11b8bc4d3b8e13332d3e34fb95f6
Gislany <gislany@gmail.com>
2026-07-08 21:00:01 -0300

    Adiciona perfil com variaveis

--- gislany/variaveis_js/variaveis.js
+++ gislany/variaveis_js/variaveis.js
@@ -4,7 +4,7 @@ let cidade = "Uberlândia";
 let profissao = "Project Manager";
 
 //Exercicio 2
-let texto = "Olá";
+let texto = "Vai curintiams";
 let numero = 10;
 let ativo = true;
 let vazio = null;
@@ -20,3 +20,31 @@ console.log("Nome:", nome);
 console.log("Idade:", idade);
 console.log("Altura:", altura);
 console.log("Possui CNH:", possuiCNH);
+
+
+//Exercicio 4
+let nome = "Gislany";
+let idade = 40;
+let cidade = "Uberlândia";
+let profissao = "Project Manager";
+let empresa = "Kumulus";
+let salario = 5200;
+let estudando = true;
+
+console.log(`
+===== Meu Perfil =====
+Nome: ${nome}
+Idade: ${idade}
+Cidade: ${cidade}
+Profissão: ${profissao}
+Empresa: ${empresa}
+Salário: USD ${salario}
+Está estudando? ${estudando}
+`);
+onsole.log("Tipo da variável nome:", typeof nome);
+console.log("Tipo da variável idade:", typeof idade);
+console.log("Tipo da variável cidade:", typeof cidade);
+console.log("Tipo da variável profissao:", typeof profissao);
+console.log("Tipo da variável empresa:", typeof empresa);
+console.log("Tipo da variável salario:", typeof salario);
+console.log("Tipo da variável estudando:", typeof estudando);
