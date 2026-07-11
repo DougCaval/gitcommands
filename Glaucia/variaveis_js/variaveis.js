26235f6286b3c1ad0ecc04b35b722d5db39ad843
glaucia cecilia <glauciacecilia2@gmail.com>
2026-07-11 10:01:36 -0300

    aula variaveis

---
+++ Glaucia/variaveis_js/variaveis.js
@@ -0,0 +1,16 @@
+var msgname = "nome do aluno: "; 
+var name = "Glaucia";
+  
+var msgIdade = "idade do aluno: ";
+var idade = 30;
+
+var msgPossuiFaculdade = "possui faculdade: ";
+var possuiFaculdade = true;
+
+console.log(msgname + name);
+console.log(msgIdade + idade);
+console.log(msgPossuiFaculdade + possuiFaculdade);
+
+
+
+
