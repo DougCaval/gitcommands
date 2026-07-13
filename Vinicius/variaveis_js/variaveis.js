b18e5e4941d426f977809a6c93b1a531375cc638
Vinicius <vinicius.aqw@gmail.com>
2026-07-13 19:56:23 -0300

    Atividades concluídas

--- Vinicius/variaveis_js/variaveis.js
+++ Vinicius/variaveis_js/variaveis.js
@@ -1,6 +1,42 @@
-let name = 'Vinicius'
-let idade = 27
-let cidade = 'Recife'
-let profissão = 'Designer'
+/* Primeira Atividade */
 
-console.log(name, idade, cidade, profissão) 
+let name = "Vinicius"
+let idade = 27
+let cidade = "Recife"
+let profissão = "Designer"
+let empresa = "FuiAli Viagens"
+let salario = 2500.00
+let estaEstudando = false
+
+console.log("\n" + name + "\n" + idade + "\n" + cidade + "\n" + profissão + "\n")
+
+/* Segunda Atividade */
+
+let possuiCNH = false;
+let nulo = null;
+let meudado;
+
+console.log(
+typeof name + "\n" + 
+typeof idade + "\n" +
+typeof possuiCNH + "\n" +
+typeof nulo + "\n" +
+typeof meudado + "\n"
+)
+
+/* Terceira Atividade */
+
+console.log("nome: " + name + "\n" + "idade: " + idade + "\n" + "possuiCNH: " + possuiCNH + "\n")
+
+/* Quarta Atividade */
+
+console.log(`Nome: ${name}\nIdade: ${idade}\nCidade: ${cidade}\nProfissão: ${profissão}\nEmpresa: ${empresa}\nSalário: R\$${salario.toFixed(2)}\nEstá estudando? ${estaEstudando}\n`)
+
+console.log(typeof name + "\n" +
+typeof idade + "\n" +
+typeof cidade + "\n" +
+typeof profissão + "\n" +
+typeof empresa + "\n" +
+typeof salario + "\n" +
+typeof estaEstudando + "\n")
+
