a82599c2972276b49b8934d5306aba6e63458322
Kalel <kdzbr77@gmail.com>
2026-07-13 19:55:43 -0300

    exercicio

--- kalelreis/variaveis_js/variaveis.js
+++ kalelreis/variaveis_js/variaveis.js
@@ -1,1 +1,41 @@
-var name = "Kalel Reis"
+let nome = "Kalel"
+let idade = "16"
+let cidade = "Uberlandia"
+let profissao = "Desenvolvedor de Jogos"
+console.log("--Primeiro Desafio--")
+console.log("Meu nome e "+ nome +" e eu tenho "+ idade +" anos, moro em "+ cidade +" e eu trabalho como "+ profissao +".")
+
+let pais = "Espanha"
+let altura = "1.62"
+let ano = 2030
+let gostaDeFrutas = false
+let gostaDeFrutasoutput = gostaDeFrutas ? "gosto" : "nao gosto"
+let meuvoto = null
+let treino;
+
+console.log("--Segundo Desafio--")
+console.log(typeof pais)
+console.log(typeof ano)
+console.log(typeof gostaDeFrutas)
+console.log(typeof meuvoto)
+console.log(typeof treino)
+
+let possuiCNH = false
+let possuiCNHoutput = possuiCNH ? "possuo" : "nao possuo"
+console.log("--Terceiro Desafio--")
+console.log("Eu "+ possuiCNHoutput +" CNH.")
+console.log("Nome: "+ nome +".")
+console.log("Idade: "+ idade +".")
+console.log("Altura: "+ altura +".")
+console.log("--Desafio da Aula--")
+let estaEstudando = true
+let estaEstudandooutput = estaEstudando ? "estou" : "nao estou"
+let empresa = "Sindux Studios"
+console.log("Nome: "+ nome +".")
+console.log("Idade: "+ idade +".")
+console.log("Cidade: "+ cidade +".")
+console.log("Profissao: "+ profissao +".")
+console.log("Empresa: "+ empresa +".")
+console.log("Eu "+ estaEstudandooutput +" estudando.")
+
+
