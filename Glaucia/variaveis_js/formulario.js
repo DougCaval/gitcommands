2aff31cc91850c48581e8ae6ca927f2ad68c8789
glaucia cecilia <glauciacecilia2@gmail.com>
2026-07-16 11:43:31 -0300

    Adiciona formulario de cadastro

---
+++ Glaucia/variaveis_js/formulario.js
@@ -0,0 +1,27 @@
+const titulo = "*Formulário de Cadastro*";
+const nome = "*Nome completo:*";
+const valorNome = "Douglas Henrique Cavalcante Martins Reis";
+const email = "*E-mail:*";
+const valorEmail = "douglas@email.com";
+const telefone = "*Telefone:*";
+const valorTelefone = "(34) 99999-9999";
+const empresa = "*Empresa:*";
+const valorEmpresa = "L4VAC IT TECH";
+const cargo = "*Cargo:*";
+const valorCargo = "QA / SDET Specialist";
+const msg = "*Mensagem / Observações:*";
+const valorMsg = "Tenho experiência na área de Qualidade de Software, atuando com testes manuais e automatizados, desenvolvimento de frameworks de automação e implementação de boas práticas de qualidade durante o ciclo de desenvolvimento de software.";
+const data = "*Data:*";
+const valorData = "11/07/2026";
+const assinatura = "*Assinatura:*";
+const valorAssinatura = "Douglas Henrique";
+console.log(`${titulo}
+${nome} ${valorNome}
+${email} ${valorEmail}
+${telefone} ${valorTelefone}
+${empresa} ${valorEmpresa}
+${cargo} ${valorCargo}
+${msg}
+${valorMsg}
+${data} ${valorData}
+${assinatura} ${valorAssinatura}`);
